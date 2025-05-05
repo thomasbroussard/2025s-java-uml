bc4934868319ac91a6174f57b85e0e738802a8fe
Présentation <tbr@tbr.com>
2025-05-05 09:26:54 +0200

    scenario implementation : first step

--- src/fr/epita/bank/datamodel/SavingsAccount.java
+++ src/fr/epita/bank/datamodel/SavingsAccount.java
@@ -2,4 +2,11 @@ package fr.epita.bank.datamodel;
 
 public class SavingsAccount extends Account{
     Double interestRate;
+
+    public SavingsAccount(Double balance, Customer customer, Double interestRate){
+        this.balance = balance;
+        this.customer = customer;
+        this.interestRate = interestRate;
+    }
+
 }
--- src/fr/epita/bank/launcher/Main.java
+++ src/fr/epita/bank/launcher/Main.java
@@ -1,18 +1,25 @@
 package fr.epita.bank.launcher;
 
 import fr.epita.bank.datamodel.Customer;
+import fr.epita.bank.datamodel.SavingsAccount;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("welcome to this bank application");
         Customer someCustomer = new Customer("thomas", "Paris");
+
         someCustomer.setName("thomas");
         someCustomer.setAddress("Paris");
 
         // I would like to have a small scenario where customer creates 2 accounts
-        // 1 savings account: initial balance: 500€, intereste rate is 3%
+        // 1 savings account: initial balance: 500€, interest rate is 3%
+        SavingsAccount savingsAccount = new SavingsAccount(500.0, someCustomer, 0.03);
+
+
         // 1 investment account: initial balance : 1000€
+
+
         // the customer withdraws 200€ from the savings
         // the customer buys 5 stocks of gold (unit price: 100€) using his investment account
         // the commission is 5% of all transactions
