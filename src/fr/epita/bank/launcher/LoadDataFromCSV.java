7d53ff73112bac358bce89a060214ab0ef009b57
Présentation <tbr@tbr.com>
2025-05-12 12:00:09 +0200

    loading data from csv

---
+++ src/fr/epita/bank/launcher/LoadDataFromCSV.java
@@ -0,0 +1,14 @@
+package fr.epita.bank.launcher;
+
+public class LoadDataFromCSV {
+
+    public static void main(String[] args) {
+        //1. load all the data from csv under 4 different lists, do not consider the links at this step
+        //2. now load data in the right order, and try to assign the corresponding data to each instance
+        // for instance: account 1 is linked to customer 1 account1.setCustomer(customer1)
+        //3. display the whole graph of data (HARD)
+
+
+
+    }
+}
